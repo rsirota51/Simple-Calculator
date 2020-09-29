ed9fc2dac429888115e5612846c52f17249a8194
rsirota51 <47458919+rsirota51@users.noreply.github.com>
2020-09-29 16:10:32 -0400

    Added Files

---
+++ Calculator 2/Calculator 2/Form1.Designer.vb
@@ -0,0 +1,382 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtDisplay = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnOff = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btnDot = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btnEqual = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.btnTimes = New System.Windows.Forms.Button()
+        Me.btnMinus = New System.Windows.Forms.Button()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.btnIntDivsion = New System.Windows.Forms.Button()
+        Me.btnMod = New System.Windows.Forms.Button()
+        Me.btnSquareRoot = New System.Windows.Forms.Button()
+        Me.btnRndNum = New System.Windows.Forms.Button()
+        Me.btnPercent = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtDisplay
+        '
+        Me.txtDisplay.Location = New System.Drawing.Point(36, 47)
+        Me.txtDisplay.Name = "txtDisplay"
+        Me.txtDisplay.Size = New System.Drawing.Size(234, 20)
+        Me.txtDisplay.TabIndex = 0
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.Blue
+        Me.btnClear.Location = New System.Drawing.Point(36, 93)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(64, 23)
+        Me.btnClear.TabIndex = 1
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'btnOff
+        '
+        Me.btnOff.BackColor = System.Drawing.Color.Blue
+        Me.btnOff.Location = New System.Drawing.Point(107, 92)
+        Me.btnOff.Name = "btnOff"
+        Me.btnOff.Size = New System.Drawing.Size(57, 23)
+        Me.btnOff.TabIndex = 2
+        Me.btnOff.Text = "Off"
+        Me.btnOff.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.Color.Red
+        Me.btn7.ForeColor = System.Drawing.Color.Black
+        Me.btn7.Location = New System.Drawing.Point(36, 174)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(41, 23)
+        Me.btn7.TabIndex = 3
+        Me.btn7.Tag = "7"
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.Color.Red
+        Me.btn3.ForeColor = System.Drawing.Color.Black
+        Me.btn3.Location = New System.Drawing.Point(130, 232)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(41, 23)
+        Me.btn3.TabIndex = 4
+        Me.btn3.Tag = "3"
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.Color.Red
+        Me.btn2.ForeColor = System.Drawing.Color.Black
+        Me.btn2.Location = New System.Drawing.Point(83, 232)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(41, 23)
+        Me.btn2.TabIndex = 5
+        Me.btn2.Tag = "2"
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.Color.Red
+        Me.btn1.ForeColor = System.Drawing.Color.Black
+        Me.btn1.Location = New System.Drawing.Point(36, 232)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(41, 23)
+        Me.btn1.TabIndex = 6
+        Me.btn1.Tag = "1"
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.Color.Red
+        Me.btn6.ForeColor = System.Drawing.Color.Black
+        Me.btn6.Location = New System.Drawing.Point(130, 204)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(41, 23)
+        Me.btn6.TabIndex = 7
+        Me.btn6.Tag = "6"
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.Color.Red
+        Me.btn5.ForeColor = System.Drawing.Color.Black
+        Me.btn5.Location = New System.Drawing.Point(83, 203)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(41, 23)
+        Me.btn5.TabIndex = 8
+        Me.btn5.Tag = "5"
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.Color.Red
+        Me.btn4.ForeColor = System.Drawing.Color.Black
+        Me.btn4.Location = New System.Drawing.Point(36, 203)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(41, 23)
+        Me.btn4.TabIndex = 9
+        Me.btn4.Tag = "4"
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.Color.Red
+        Me.btn9.ForeColor = System.Drawing.Color.Black
+        Me.btn9.Location = New System.Drawing.Point(130, 175)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(41, 23)
+        Me.btn9.TabIndex = 10
+        Me.btn9.Tag = "9"
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.Color.Red
+        Me.btn8.ForeColor = System.Drawing.Color.Black
+        Me.btn8.Location = New System.Drawing.Point(83, 175)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(41, 23)
+        Me.btn8.TabIndex = 11
+        Me.btn8.Tag = "8"
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btnDot
+        '
+        Me.btnDot.BackColor = System.Drawing.Color.Red
+        Me.btnDot.ForeColor = System.Drawing.Color.Black
+        Me.btnDot.Location = New System.Drawing.Point(83, 261)
+        Me.btnDot.Name = "btnDot"
+        Me.btnDot.Size = New System.Drawing.Size(41, 23)
+        Me.btnDot.TabIndex = 12
+        Me.btnDot.Tag = "."
+        Me.btnDot.Text = "."
+        Me.btnDot.UseVisualStyleBackColor = False
+        '
+        'btn0
+        '
+        Me.btn0.BackColor = System.Drawing.Color.Red
+        Me.btn0.ForeColor = System.Drawing.Color.Black
+        Me.btn0.Location = New System.Drawing.Point(36, 261)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(41, 23)
+        Me.btn0.TabIndex = 13
+        Me.btn0.Tag = "0"
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = False
+        '
+        'btnEqual
+        '
+        Me.btnEqual.BackColor = System.Drawing.Color.Red
+        Me.btnEqual.ForeColor = System.Drawing.Color.Black
+        Me.btnEqual.Location = New System.Drawing.Point(130, 261)
+        Me.btnEqual.Name = "btnEqual"
+        Me.btnEqual.Size = New System.Drawing.Size(57, 23)
+        Me.btnEqual.TabIndex = 14
+        Me.btnEqual.Tag = "="
+        Me.btnEqual.Text = "="
+        Me.btnEqual.UseVisualStyleBackColor = False
+        '
+        'btnDivide
+        '
+        Me.btnDivide.BackColor = System.Drawing.Color.Lime
+        Me.btnDivide.Location = New System.Drawing.Point(248, 178)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(41, 23)
+        Me.btnDivide.TabIndex = 15
+        Me.btnDivide.Tag = "/"
+        Me.btnDivide.Text = "/"
+        Me.btnDivide.UseVisualStyleBackColor = False
+        '
+        'btnTimes
+        '
+        Me.btnTimes.BackColor = System.Drawing.Color.Lime
+        Me.btnTimes.Location = New System.Drawing.Point(248, 149)
+        Me.btnTimes.Name = "btnTimes"
+        Me.btnTimes.Size = New System.Drawing.Size(41, 23)
+        Me.btnTimes.TabIndex = 16
+        Me.btnTimes.Tag = "*"
+        Me.btnTimes.Text = "*"
+        Me.btnTimes.UseVisualStyleBackColor = False
+        '
+        'btnMinus
+        '
+        Me.btnMinus.BackColor = System.Drawing.Color.Lime
+        Me.btnMinus.Location = New System.Drawing.Point(248, 120)
+        Me.btnMinus.Name = "btnMinus"
+        Me.btnMinus.Size = New System.Drawing.Size(41, 23)
+        Me.btnMinus.TabIndex = 17
+        Me.btnMinus.Tag = "-"
+        Me.btnMinus.Text = "-"
+        Me.btnMinus.UseVisualStyleBackColor = False
+        '
+        'btnPlus
+        '
+        Me.btnPlus.BackColor = System.Drawing.Color.Lime
+        Me.btnPlus.Location = New System.Drawing.Point(248, 91)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(41, 23)
+        Me.btnPlus.TabIndex = 18
+        Me.btnPlus.Tag = "+"
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = False
+        '
+        'btnIntDivsion
+        '
+        Me.btnIntDivsion.BackColor = System.Drawing.Color.Lime
+        Me.btnIntDivsion.Location = New System.Drawing.Point(248, 207)
+        Me.btnIntDivsion.Name = "btnIntDivsion"
+        Me.btnIntDivsion.Size = New System.Drawing.Size(41, 23)
+        Me.btnIntDivsion.TabIndex = 19
+        Me.btnIntDivsion.Tag = "\"
+        Me.btnIntDivsion.Text = "\"
+        Me.btnIntDivsion.UseVisualStyleBackColor = False
+        '
+        'btnMod
+        '
+        Me.btnMod.BackColor = System.Drawing.Color.Lime
+        Me.btnMod.Location = New System.Drawing.Point(248, 236)
+        Me.btnMod.Name = "btnMod"
+        Me.btnMod.Size = New System.Drawing.Size(41, 23)
+        Me.btnMod.TabIndex = 20
+        Me.btnMod.Tag = "Mod"
+        Me.btnMod.Text = "Mod"
+        Me.btnMod.UseVisualStyleBackColor = False
+        '
+        'btnSquareRoot
+        '
+        Me.btnSquareRoot.BackColor = System.Drawing.Color.Lime
+        Me.btnSquareRoot.Location = New System.Drawing.Point(248, 265)
+        Me.btnSquareRoot.Name = "btnSquareRoot"
+        Me.btnSquareRoot.Size = New System.Drawing.Size(41, 23)
+        Me.btnSquareRoot.TabIndex = 21
+        Me.btnSquareRoot.Tag = " 11"
+        Me.btnSquareRoot.Text = " √a"
+        Me.btnSquareRoot.UseVisualStyleBackColor = False
+        '
+        'btnRndNum
+        '
+        Me.btnRndNum.BackColor = System.Drawing.Color.Lime
+        Me.btnRndNum.Location = New System.Drawing.Point(201, 91)
+        Me.btnRndNum.Name = "btnRndNum"
+        Me.btnRndNum.Size = New System.Drawing.Size(41, 23)
+        Me.btnRndNum.TabIndex = 22
+        Me.btnRndNum.Tag = "Rnd"
+        Me.btnRndNum.Text = "Rnd"
+        Me.btnRndNum.UseVisualStyleBackColor = False
+        '
+        'btnPercent
+        '
+        Me.btnPercent.BackColor = System.Drawing.Color.Lime
+        Me.btnPercent.Location = New System.Drawing.Point(201, 120)
+        Me.btnPercent.Name = "btnPercent"
+        Me.btnPercent.Size = New System.Drawing.Size(41, 23)
+        Me.btnPercent.TabIndex = 23
+        Me.btnPercent.Tag = "%"
+        Me.btnPercent.Text = "%"
+        Me.btnPercent.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Yellow
+        Me.ClientSize = New System.Drawing.Size(301, 306)
+        Me.Controls.Add(Me.btnPercent)
+        Me.Controls.Add(Me.btnRndNum)
+        Me.Controls.Add(Me.btnSquareRoot)
+        Me.Controls.Add(Me.btnMod)
+        Me.Controls.Add(Me.btnIntDivsion)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.btnMinus)
+        Me.Controls.Add(Me.btnTimes)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnEqual)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnDot)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btn7)
+        Me.Controls.Add(Me.btnOff)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.txtDisplay)
+        Me.Name = "Form1"
+        Me.Text = "Calculator 2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtDisplay As System.Windows.Forms.TextBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnOff As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btnDot As System.Windows.Forms.Button
+    Friend WithEvents btn0 As System.Windows.Forms.Button
+    Friend WithEvents btnEqual As System.Windows.Forms.Button
+    Friend WithEvents btnDivide As System.Windows.Forms.Button
+    Friend WithEvents btnTimes As System.Windows.Forms.Button
+    Friend WithEvents btnMinus As System.Windows.Forms.Button
+    Friend WithEvents btnPlus As System.Windows.Forms.Button
+    Friend WithEvents btnIntDivsion As System.Windows.Forms.Button
+    Friend WithEvents btnMod As System.Windows.Forms.Button
+    Friend WithEvents btnSquareRoot As System.Windows.Forms.Button
+    Friend WithEvents btnRndNum As System.Windows.Forms.Button
+    Friend WithEvents btnPercent As System.Windows.Forms.Button
+
+End Class
---
+++ Calculator 2/Calculator 2/Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class Form1
+    'Robert Sirota
+    '4/22/14
+    Dim operand1 As Double = 0
+    Dim operand2 As Double = 0
+    Dim op As String = Nothing
+    Dim newOperand As Boolean = True
+
+    Private Sub Number_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnDot.Click, btn0.Click, btn1.Click, btn2.Click, _
+    btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+        Dim btnNumberClicked As Button = sender
+        If newOperand Then
+            Me.txtDisplay.Text = btnNumberClicked.Tag
+            newOperand = False
+        Else
+            Me.txtDisplay.Text &= btnNumberClicked.Tag
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnClear.Click
+        Me.txtDisplay.Text = "0"
+        operand1 = 0
+        operand2 = 0
+        newOperand = True
+        op = Nothing
+    End Sub
+
+    Private Sub btnOff_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnOff.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub btnOperator_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnPlus.Click, btnMinus.Click, btnTimes.Click, _
+    btnDivide.Click, btnEqual.Click, btnIntDivsion.Click, btnMod.Click, btnSquareRoot.Click, btnRndNum.Click, btnPercent.Click
+        Dim decimalstring As String = "."
+        Dim decimalcount As Integer
+        Dim TextCheck As String = Me.txtDisplay.Text
+        For index As Integer = 0 To TextCheck.Length() - 1
+            If TextCheck.Chars(index) = decimalstring Then
+                decimalcount += 1
+            ElseIf decimalcount = 2 Then
+                Me.txtDisplay.Text = "ERROR"
+            End If
+        Next
+        Dim operatorSelected As Button = sender
+        If (operand1 = 0 And op = Nothing) Or op = "=" Then
+            operand1 = Val(Me.txtDisplay.Text)
+        Else
+            operand2 = Val(Me.txtDisplay.Text)
+            operand1 = Calculate(operand1, operand2, op)
+            Me.txtDisplay.Text = operand1
+        End If
+        op = operatorSelected.Tag
+        newOperand = True
+    End Sub
+
+    Function Calculate(ByVal firstOperand As Double, ByVal secondOperand As Double, ByVal op As String) As Double
+        Select Case op
+            Case "+"
+                Return (firstOperand + secondOperand)
+            Case "-"
+                Return (firstOperand - secondOperand)
+            Case "*"
+                Return (firstOperand * secondOperand)
+            Case "/"
+                If secondOperand = 0 Then
+                    Me.txtDisplay.Text = "ERROR"
+                End If
+                Return (firstOperand / secondOperand)
+            Case "\"
+                Return (firstOperand \ secondOperand)
+            Case "Mod"
+                Return (firstOperand Mod secondOperand)
+            Case "11"
+                If firstOperand < 0 Then
+                    firstOperand = Math.Abs(firstOperand)
+                    firstOperand = Math.Sqrt(firstOperand)
+                    Return (firstOperand & "i")
+                ElseIf firstOperand > 0 Then
+                    firstOperand = Math.Sqrt(firstOperand)
+                    Return (firstOperand)
+                End If
+            Case "Rnd"
+                If firstOperand < secondOperand Then
+                    Me.txtDisplay.Text = "Chose Highest Number First"
+                End If
+                Return ((firstOperand - secondOperand + 1) * Rnd() + secondOperand)
+            Case "%"
+                Return (firstOperand * 100)
+        End Select
+    End Function
+End Class
